52b7ce98a965b1902146e513fb0bbbb0afffe83f
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-13 15:49:08 -0400

    Configurações iniciais.

---
+++ backend/src/app.ts
@@ -0,0 +1,4 @@
+import express from 'express'
+const app = express()
+
+app.listen(3030, () => console.log(`🚀 Server is running on PORT 3003`))
--- backend/src/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-const app = express()
-
-app.listen(3030, () => console.log(
-    'Server Online!'
-))

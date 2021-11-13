f3465271089646d3906ff5f97bca3132a5e9d5e7
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-13 17:20:54 -0400

    "feat": Created a connection with tsc-node.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,12 @@
 import express from 'express'
+import { router } from './routes'
+import http from 'http'
+import cors from 'cors'
+
 const app = express()
 
-app.listen(3030, () => console.log(`🚀 Server is running on PORT 3003`))
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(3003, () => console.log('🚀 Server Online'))

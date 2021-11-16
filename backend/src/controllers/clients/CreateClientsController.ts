873e56d1c90abe6a162878bc58026860b3ca8b68
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-15 23:00:20 -0400

    "refact": Create a interface.

--- backend/src/controllers/clients/CreateClientsController.ts
+++ backend/src/controllers/clients/CreateClientsController.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from 'express'
 import { CreateClientsService } from '../../services/clients/CreateClientsService'
 
+interface IRequest {
+  name: string
+  cnpj_cpf: string
+  email: string
+  phone: string
+  address: string
+  number: string
+  district: string
+  city: string
+  state: string
+  cep: number
+}
 class CreateClientsController {
   async handle(request: Request, response: Response) {
     const {
@@ -14,8 +26,7 @@ class CreateClientsController {
       city,
       state,
       cep,
-    } = request.body
-
+    }: IRequest = request.body
     const service = new CreateClientsService()
     const result = await service.execute(
       name,
@@ -29,7 +40,6 @@ class CreateClientsController {
       state,
       cep
     )
-
     return response.json(result)
   }
 }

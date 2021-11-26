527d285ccdec21b3b50e79d6c63beafbcff577fd
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-25 22:37:55 -0400

    "feat": ajusts route delete

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { ListClientsController } from './controllers/clients/ListClientsController'
 import { CreateClientsController } from './controllers/clients/CreateClientsController'
 import { EditClientsController } from './controllers/clients/EditClientsController'
+import { DeleteClientsController } from './controllers/clients/DeleteClientsController'
+import { clientValidator } from './middlewares/validatorClient'
 const router = Router()
 
 router.get('/clients', new ListClientsController().handle)
-router.post('/clients', new CreateClientsController().handle)
+router.post('/clients', clientValidator, new CreateClientsController().handle)
 router.put('/clients', new EditClientsController().handle)
-router.delete('/clients', new EditClientsController().handle)
+router.delete('/clients', new DeleteClientsController().handle)
 
 export { router }

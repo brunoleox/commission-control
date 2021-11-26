f5dd1c361d1eafa7f2c2638fff3c16bbcfa2cac6
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-25 22:38:31 -0400

    "feat": created a controller AppError

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,26 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import { router } from './routes'
-import http from 'http'
+import { AppError } from './errors/AppError'
 import cors from 'cors'
+import 'express-async-errors'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message} `,
+    })
+  }
+)
 
 app.listen(3003, () => console.log('🚀 Server Online'))

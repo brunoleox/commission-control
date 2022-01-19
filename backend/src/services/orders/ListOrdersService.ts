6451c13ecc41c7ef6b1a0b6f57b280b6881f0fba
Bruno <bruno_leonardosilva@hotmail.com>
2022-01-19 00:48:02 -0400

    chore: edit archive indentation

--- backend/src/services/orders/ListOrdersService.ts
+++ backend/src/services/orders/ListOrdersService.ts
@@ -3,10 +3,11 @@ import prismaCLient from '../../prisma'
 class ListOrdersService {
   async execute() {
     const listOrders = await prismaCLient.order.findMany({
-      include: { products: true },
+      include: {
+        products: true,
+      },
     })
     return listOrders
   }
 }
-
 export { ListOrdersService }

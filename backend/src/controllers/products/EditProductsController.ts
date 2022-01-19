6220d645ff10b19a2027403a95662f241f5f2a33
Bruno <bruno_leonardosilva@hotmail.com>
2022-01-19 00:46:51 -0400

    chore: edit error provider in products controller

--- backend/src/controllers/products/EditProductsController.ts
+++ backend/src/controllers/products/EditProductsController.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express'
 import { IProducts } from '../../interfaces'
-import { EditProductsService } from '../../services/products/EditProdutsService'
+import { EditProductsService } from '../../services/products/EditProductsService'
+import { AppErrorProvider } from '../../utils/AppErrorProvider'
 
 class EditProductsController {
   async handle(request: Request, response: Response) {
-    const { ...products }: IProducts = request.body
+    try {
+      const { ...products }: IProducts = request.body
 
-    const service = new EditProductsService()
-    const result = await service.execute({ ...products })
-    return response.json(result)
+      const service = new EditProductsService()
+      const result = await service.execute({ ...products })
+      return response.json(result)
+    } catch (error) {
+      AppErrorProvider('Algo deu errado ao editar o produto!', response, error)
+    }
   }
 }
 

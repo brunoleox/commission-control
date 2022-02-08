8568ac40e8dcda320499b06fca0e1857e42f8c14
Bruno <bruno_leonardosilva@hotmail.com>
2022-02-08 18:44:29 -0400

    feat: added password hash

--- backend/src/services/users/CreateUsersService.ts
+++ backend/src/services/users/CreateUsersService.ts
@@ -1,7 +1,7 @@
-import { isValidCNPJ, isValidCPF } from '@brazilian-utils/brazilian-utils'
 import { AppError } from '../../errors/AppError'
 import { IUsers } from '../../interfaces'
 import prismaCLient from '../../prisma'
+import { hash } from 'bcrypt'
 class CreateUsersService {
   async execute({ ...users }: IUsers) {
     const userAlreadExists = await prismaCLient.user.findUnique({
@@ -12,9 +12,13 @@ class CreateUsersService {
     if (userAlreadExists) {
       throw new AppError('O email informado já está em uso.')
     }
+
+    const passwordHash = await hash(users.password, 8)
+
     const createUser = await prismaCLient.user.create({
       data: {
         ...users,
+        password: passwordHash,
       },
     })
     return createUser

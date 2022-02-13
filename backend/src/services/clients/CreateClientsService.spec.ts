7dbdd9aa3eb24aeea9a8237f5b867b1b5825b3cb
Bruno <bruno_leonardosilva@hotmail.com>
2022-02-12 21:20:03 -0400

    fix: removed test file

--- backend/src/services/clients/CreateClientsService.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateClientsService } from './CreateClientsService'
-
-describe('Create client', () => {
-  it('should be able to create a new client', () => {
-    const createClient = new CreateClientsService()
-  })
-})
